4ab65c5ee2b9f237bb50109780d0dd7161f705ce
Charlotte Wainwright <charlotte.wainwright@sparck.io>
2019-09-27 09:15:50 +0100

    marketing page amends

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -50,21 +50,3 @@ function checkCookieOnLoad() {
 window.onload = function() {
   checkCookieOnLoad();
 };
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickyContents()};
-
-// Get the header
-var header = document.getElementById("contents");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyContents() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
