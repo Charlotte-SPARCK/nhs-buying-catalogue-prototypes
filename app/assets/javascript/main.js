cca824a71a0aa5d2c2fafa6e30b8bddcc6e39971
Charlotte Wainwright <charlotte.wainwright@sparck.io>
2019-10-24 00:36:09 +0100

    buyer-browse amends

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -7,6 +7,7 @@ function checkboxLink() {
   var digitalDiagnostics2 = document.getElementById("digital-diagnostics-2");
   var productivityTheme = document.getElementById("professional-to-professional-1");
   var noResults = document.getElementById("shared-care-plan");
+  var noResults2 = document.getElementById("professional-to-professional");
 
   if (digitalDiagnostics.checked == true) {
     document.getElementById("view-results").setAttribute("href", "/buyer-browse-v2/capability-select/capability-search-results");
@@ -18,6 +19,9 @@ function checkboxLink() {
     document.getElementById("view-results").setAttribute("href", "/buyer-browse-v2/productivity/productivity-search-results");
   }
   else if (noResults.checked == true) {
+    document.getElementById("view-results").setAttribute("href", "no-results");
+  }
+  else if (noResults2.checked == true) {
     document.getElementById("view-results").setAttribute("href", "no-results");
   }
   else {
